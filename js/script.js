7987466ca1b3b873d2af10f38334241d925ed61b
Matteo <matteop18@gmail.com>
2020-02-03 18:17:28 +0100

    sistemata prima parte ora fare chiamata ajax

--- js/script.js
+++ js/script.js
@@ -1,40 +1,32 @@
 $(document).ready(function() {
-  var url = 'https://flynn.boolean.careers/exercises/api/holidays?year=2018&month=0';
-  // Chiammata gennaio
-$.ajax(
-  {
-    'url': url,
-    'method': 'GET',
-    'success': function(data) {
-      console.log(data);
-      // inserisco in una variabile i giorni del mese
-      var january = moment(2018).format("MMMM");
-      console.log(january);
-      var date = moment(2018).daysInMonth();
-      console.log(date);
-      for (var i = 1; i < date + 1; i++) {
-        console.log(i);
-        var source = $("#entry-template").html();
-        var template = Handlebars.compile(source);
-        var context = {
-          'month': january,
-          'day': i
-        };
-        var html = template(context);
-        $('#calendar').append(html);
-      }
-    },
-    'error': function(request, state, error) {
-      console.log('error' + error);
-    }
+  var month = moment("2018-01");
+  // console.log(month);
+  var days = month.daysInMonth();
+  // console.log(days);
+  for (var i = 0; i < days; i++) {
+   num = (i + 1);
+   var numZ = zero(num)
+   // console.log(numZ);
+   var date = moment("2018-01" + "-" + numZ);
+   // console.log(date);
+   var fullDate = date.format("YYYY-MM-DD");
+   console.log(fullDate);
+   var dayMonth = date.format("DD MMMM");
+   // console.log(dayMonth);
+   var source = $("#entry-template").html();
+   var template = Handlebars.compile(source);
+   var context = {
+     'dayMonth': dayMonth,
+     'dayDate': fullDate
+   };
+   var html = template(context);
+   $("#calendar").append(html);
   }
-);
-
-
-
-
-
-
-
-
 });
+
+function zero(num) {
+  if (num < 10) {
+    num = "0" + num;
+  }
+  return num;
+}
